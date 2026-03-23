97a4fb221f92a5d60e3bfe157a34d85d01aa6a2e
Stefaniazb <stefania.bolivar@escola.pr.gov.br>
2026-03-23 13:27:44 +0000

    Finalização do objetos js

---
+++ objetos/obj-02.js
@@ -0,0 +1,24 @@
+// Crie um objeto do tema que escolher
+// Crie pelo menos 5 propriedades
+// Faça uma impressão para Humanos!
+// ex. console.log("Nome do Jogador: " + jogador.nome)
+
+const Panificadora = {
+    Nome: "Boa Vista",
+    Local: "Boa Vista",
+    Vagas: ["Balconista", "Padeiro", "Limpeza"],
+    HoráriodeAtendimento: "Segunda a sábado, das 06h às 20h, e aos domingos, das 07h às 19h",
+    DatadeFundação: "1996"
+}
+Panificadora.nome = "Boa Vista"
+Panificadora.Local = "Boa Vista"
+Panificadora.Vagas = "Balconista", "Padeiro", "Limpeza"
+Panificadora.HoráriodeAtendimento = "Segunda a sábado, das 06h às 20h, e aos domingos, das 07h às 19h"
+Panificadora.DatadeFundação = "1996"
+
+console.log(Panificadora.nome)
+console.log(Panificadora.Local)
+console.log(Panificadora.Vagas)
+console.log(Panificadora.HoráriodeAtendimento)
+console.log(Panificadora.DatadeFundação)
+console.log("Boa Vista: " + Panificadora.Local)
