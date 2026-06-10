221b4fc711d3a90092580de78c00cafff7541474
Stefaniazb <stefania.bolivar@escola.pr.gov.br>
2026-06-10 11:50:17 +0000

    Módulo Fs + tryCatch + chalk para terminal

--- 2°Trimestre/modulos/mod03-FS/fs-com-path.js
+++ 2°Trimestre/modulos/mod03-FS/fs-com-path.js
@@ -1,9 +1,21 @@
-// importar PAHT E FS 
+// importar PATHE E FS 
+// Path possui (join para jutar caminhos e Resolve
+// para deduzir subdiretórios e S.O.)
+const { default: chalk } = require('chalk')
+const fs = require('fs')
+const path = require('path')
 
+// Salvar o Caminho em um variável 
+//const caminho = path.join(__dirname, 'README.md')
+try {
+const caminho = path.resolve(__dirname, 'READM.md') // Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync(caminho, 'utf-8')
 
+console.log(arquivo)
+} catch(erro){
+    console.log('--------------------')
+    console.warn("Atenção!"+erro.path)
+    throw new Error('Mensagem de erro personalizada');
 
-// Salvar o Caminho em uma variável
-
-
-
-// Abrir, ler, deletar, editar
+    console.log('--------------------')
+}
